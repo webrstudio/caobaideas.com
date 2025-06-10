966fad250966de2618a7c26567273c8aae2e4885
webrstudio <contacto@webrstudio.com>
2025-06-09 20:32:58 -0600

    se agrega el apartado dinamico de noticias

--- src/app/(inicio)/components/home/Notices/Notices.jsx
+++ src/app/(inicio)/components/home/Notices/Notices.jsx
@@ -3,6 +3,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { useContext } from "react";
+import { useFetch } from "@/hooks";
 import styles from "./styles.module.css";
 import { ThemeContext } from "@/contexts";
 import { Slide } from "react-awesome-reveal";
@@ -12,9 +13,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 export const Notices = () => {
-  const { lightMode } = useContext(ThemeContext)
+  const { lightMode } = useContext(ThemeContext);
+  const { isLoading, data } = useFetch({
+    url: `${process.env.NEXT_PUBLIC_API}/posts/getAllPosts/index.php`,
+  });
   return (
-    <Container bgClassName={!lightMode ? 'bgBlackSecondary' : 'bgWhiteSecondary'}>
+    <Container
+      bgClassName={!lightMode ? "bgBlackSecondary" : "bgWhiteSecondary"}
+    >
       <Title title="Últimas noticias" />
       <Slide triggerOnce>
         <div className={`${styles.noticesWrapper} smallContainer`}>
@@ -26,31 +32,28 @@ export const Notices = () => {
             modules={[Navigation, Pagination, Autoplay]}
             navigation
             pagination={{ clickable: true }}
-            spaceBetween={100}
+            spaceBetween={50}
             slidesPerView={1}
             autoplay={{ delay: 2000, disableOnInteraction: false }}
             loop={true}
+            className={styles.noticePlaylist}
           >
-            <SwiperSlide>
-              <div className={styles.noticeWrapper}>
-                <figure className={styles.noticeImage}>
-                  <img src="https://caobaideas.com/wp-content/uploads/2025/03/canciones-de-wilyn-y-los-mesmos.png" />
-                </figure>
-                <h4 className={styles.noticeTitle}>
-                  Las canciones de Wilyn y Los Mesmos ¡ya en plataformas!
-                </h4>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className={styles.noticeWrapper}>
-                <figure className={styles.noticeImage}>
-                  <img src="https://caobaideas.com/wp-content/uploads/2025/03/canciones-de-wilyn-y-los-mesmos.png" />
-                </figure>
-                <h4 className={styles.noticeTitle}>
-                  Las canciones de Wilyn y Los Mesmos ¡ya en plataformas!
-                </h4>
-              </div>
-            </SwiperSlide>
+            {!isLoading && data
+              ? data.map((post) => (
+                  <SwiperSlide key={post.noticia_id}>
+                    <div className={styles.noticeWrapper}>
+                      <figure className={styles.noticeImage}>
+                        <img
+                          src={`${process.env.NEXT_PUBLIC_SERVER_NAME}/post-images/${post.noticia_imagen}`}
+                        />
+                      </figure>
+                      <h4 className={styles.noticeTitle}>
+                        {post.noticia_titulo}
+                      </h4>
+                    </div>
+                  </SwiperSlide>
+                ))
+              : null}
           </Swiper>
         </div>
       </Slide>
