2af87c10bc32467901abcf7b0b592dd8533ddfa4
webrstudio <contacto@webrstudio.com>
2025-06-30 20:33:06 -0600

    se llama al endpoint de everntos

--- src/app/(inicio)/components/home/Events/Events.jsx
+++ src/app/(inicio)/components/home/Events/Events.jsx
@@ -2,13 +2,19 @@
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { useFetch } from "@/hooks";
 import styles from "./styles.module.css";
 import { Slide } from "react-awesome-reveal";
 import { Container, Title } from "@/components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Link from "next/link";
 
 export const Events = () => {
+  const { isLoading, data } = useFetch({
+    url: `${process.env.NEXT_PUBLIC_API}/events/getAllEvents/index.php`,
+  });
+  console.log(data);
   return (
     <Container>
       <Title title="Eventos" />
@@ -23,21 +29,19 @@ export const Events = () => {
             autoplay={{ delay: 2000, disableOnInteraction: false }}
             loop={true}
           >
-            <SwiperSlide>
-              <figure className={`${styles.imageEvent}`}>
-                <img src={`${process.env.NEXT_PUBLIC_SERVER_NAME}/event-images/event-1.jpg`} />
-              </figure>
-            </SwiperSlide>
-            <SwiperSlide>
-              <figure className={styles.imageEvent}>
-                <img src={`${process.env.NEXT_PUBLIC_SERVER_NAME}/event-images/event-2.jpg`} />
-              </figure>
-            </SwiperSlide>
-            <SwiperSlide>
-              <figure className={styles.imageEvent}>
-                <img src={`${process.env.NEXT_PUBLIC_SERVER_NAME}/event-images/event-3.jpg`} />
-              </figure>
-            </SwiperSlide>
+            {isLoading && !data
+              ? null
+              : data.map((event) => (
+                  <SwiperSlide key={event.evento_id}>
+                    <a href={event.evento_link} target="_blank">
+                      <figure className={`${styles.imageEvent}`}>
+                        <img
+                          src={`${process.env.NEXT_PUBLIC_SERVER_NAME}/event-images/${event.evento_imagen}`}
+                        />
+                      </figure>
+                    </a>
+                  </SwiperSlide>
+                ))}
           </Swiper>
         </div>
       </Slide>
